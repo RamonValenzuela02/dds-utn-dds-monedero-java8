c3c758179c1ece123ed2e2dcb674746a3bc3fe83
Ramon Valenzuela <rvalenzuela@frba.utn.edu.ar>
2025-05-17 23:16:26 -0300

    mejoro el manejo de errores de el test tresdepositos

--- src/test/java/dds/monedero/model/MonederoTest.java
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MonederoTest {
@@ -20,8 +21,10 @@ public class MonederoTest {
 
   @Test
   @DisplayName("Es posible poner $1500 en una cuenta vacía")
-  void Poner() {
+  void PonerUnMonto() {
     cuenta.poner(1500);
+    assertEquals(1500,cuenta.getSaldo());
+    assertEquals(1,cuenta.getMovimientos().size());
   }
 
   @Test
@@ -36,6 +39,8 @@ public class MonederoTest {
     cuenta.poner(1500);
     cuenta.poner(456);
     cuenta.poner(1900);
+    assertEquals(3856,cuenta.getSaldo());
+    assertEquals(3,cuenta.getMovimientos().size());
   }
 
   @Test
